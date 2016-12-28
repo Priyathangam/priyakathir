edc3a3d695572e1637ad07d2a88f086a5fdc5cb6
Priyathangam <priyathangam96@gmail.com>
2016-12-28 14:20:50 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    printf("\nenter the character:");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("\nthe character is vowel");
+    }
+        else
+        {
+            printf("\nthe character is a consonant");
+        }
+    getch();
+}
