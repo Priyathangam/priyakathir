30876d3e457c1501da1e41a7d58c7417bd9a367b
Priyathangam <priyathangam96@gmail.com>
2016-12-29 02:23:22 -0800

    Add files via upload

---
+++ natu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void  main()
+{
+    int n,count,i;
+    printf("\nenter the number:");
+    scanf("%d",&i);
+    printf("\nprint natural numbers from 1to%d\n",n);
+    for(count=1;count<n;count++)
+{
+    printf("%d\n",count);
+
+}
+    getch();
+}
+
