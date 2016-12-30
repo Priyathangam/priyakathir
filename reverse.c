539362f70637062672db663e971d3ad873c1e391
Priyathangam <priyathangam96@gmail.com>
2016-12-30 01:35:43 -0800

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char str,strrev,temp;
+    int i,n;
+    printf("\nenter the string:",str);
+    scanf("%d",&str);
+    while(strrev==0)
+    {
+        strcpy(n,i);
+        strcpy=strrev;
+        }
+    printf("\nthe reversed string is:");
+    puts(strrev);
+    getch();
+}
